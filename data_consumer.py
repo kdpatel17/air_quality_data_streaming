9921de125106d74b85234c17c1ecac20a8dd28fd
kdpatel17 <kpatel@rhrk.uni-kl.de>
2025-06-16 16:14:17 +0200

    documentation and Comment

--- data_consumer.py
+++ data_consumer.py
@@ -4,9 +4,19 @@ import pandas as pd
 from logger_utils import *
 
 
-# --- Data Filter Function ---
+# Function to check if the quality of a parameter's value is acceptable
 def is_quality_acceptable(parameter, value, thresholds):
-    """Return True if value falls under 'Good' or 'Moderate' for the given parameter."""
+    """
+    Return True if value falls under 'Good' or 'Moderate' for the given parameter.
+
+    Args:
+        parameter (str): The parameter to check.
+        value (str): The value of the parameter.
+        thresholds (dict): Dictionary containing threshold values for parameters.
+
+    Returns:
+        bool: True if the value is acceptable, False otherwise.
+    """
     if parameter not in thresholds:
         return False
 
@@ -22,9 +32,18 @@ def is_quality_acceptable(parameter, value, thresholds):
     return False
 
 
-# --- Data Transforming Function ---
+# Function to add a quality field to a message based on parameter and value
 def add_quality_field(message_dict, thresholds):
-    """Adds a 'quality' field to the message based on parameter and value."""
+    """
+    Adds a 'quality' field to the message based on parameter and value.
+
+    Args:
+        message_dict (dict): The message dictionary to modify.
+        thresholds (dict): Dictionary containing threshold values for parameters.
+
+    Returns:
+        dict: The modified message dictionary with a 'quality' field.
+    """
     parameter = message_dict.get("parameter")
     value = message_dict.get("value")
 
@@ -47,45 +66,45 @@ def add_quality_field(message_dict, thresholds):
     return message_dict
 
 
-# --- configuration ---
+# Load configuration from a JSON file
 with open('./config.json', 'r', encoding='utf-8') as f:
     config_data = json.load(f)
 
-# --- Read JSON File With Thresholds ---
+# Load thresholds from a JSON file
 with open('./thresholds.json', 'r', encoding='utf-8') as f:
     thresholds = json.load(f)
 
-# kafka_bootstrap = config_data["kafka"]["bootstrap"]
-kafka_bootstrap = "localhost:9092"
-kafka_topic = config_data["kafka"]["topic"]
-output_file = config_data["output"]["path"]
-log_conf = config_data["logging"]["consumer_path"]
+# Configuration parameters
+kafka_bootstrap = "localhost:9092"                                                                                      # Kafka bootstrap servers
+kafka_topic = config_data["kafka"]["topic"]                                                                             # Kafka topic to consume messages from
+output_file = config_data["output"]["path"]                                                                             # Path to save the output CSV file
+log_conf = config_data["logging"]["consumer_path"]                                                                      # Logging configuration path
 
-# --- Ensure Output Directory Exists ---
+# Ensure the output directory exists
 os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
-# --- Initiation of Logging File ---
+# Initialize logging file
 log_path = init_log_file(log_conf)
 
-# --- Kafka Consumer ---
+# Configure Kafka consumer
 consumer = KafkaConsumer(
     kafka_topic,
     bootstrap_servers=kafka_bootstrap,
-    auto_offset_reset='earliest',
-    value_deserializer=lambda m: json.loads(m.decode('utf-8')),
-    group_id='simple-consumer-group'
+    auto_offset_reset='earliest',                                                                                       # Start reading at the earliest message present in the topic
+    value_deserializer=lambda m: json.loads(m.decode('utf-8')),                                                         # Deserialize JSON messages
+    group_id='simple-consumer-group'                                                                                    # Consumer group ID
 )
 
 consumer.subscribe([kafka_topic])
 print(f"🔄 Consuming messages from topic: {kafka_topic}")
 
 
-# --- DataFrame and Message List ---
+# Initialize DataFrame and message list
 df = pd.DataFrame()
 data = []
 msg_count = 0
 
-# --- Fetch Each Data From Kafka Producer and Save as DataFrame
+# Fetch each data from Kafka producer and save as DataFrame
 try:
     for message in consumer:
         msg = message.value
@@ -94,7 +113,7 @@ try:
             msg = add_quality_field(msg, thresholds)                                                                    # transform the message
             data.append(msg)
 
-        # get 100 messages and then concat those messages to df
+        # Concatenate messages to DataFrame in batches of 100
         if len(data) >= 100:
             new_rows = pd.DataFrame(data)
             df = pd.concat([df, new_rows], ignore_index=True)
@@ -103,7 +122,9 @@ try:
             log_event(log_path, "data_consumer", "batch_save", len(new_rows), "success", output_file)
             log_event(log_path, "data_consumer", "Message Received", msg_count, "success", output_file)
             log_event(log_path, "data_consumer", "DataFrame Data", len(df), "success", output_file)
+
 except KeyboardInterrupt:
+    # Handle manual interruption
     last_fetch_data = pd.DataFrame(data)
     df = pd.concat([df, last_fetch_data], ignore_index=True)
     print(f"✅ Received {msg_count} messages. {len(df)} messages are concat to DataFrame.")
@@ -112,6 +133,7 @@ except KeyboardInterrupt:
     print("🛑 Stopping consumer...")
 
 finally:
+    # Save the DataFrame to a CSV file if it's not empty
     if not df.empty:
         df.to_csv(output_file, index=False)
         log_event(log_path, "data_consumer", "final_save", len(df), "success", output_file)
--- data_ingestion.py
+++ data_ingestion.py
@@ -5,43 +5,47 @@ from tqdm import tqdm
 import json
 from logger_utils import *
 
-# configuration
+# Load configuration from a JSON file
 with open('./config.json', 'r', encoding='utf-8') as f:
     config_data = json.load(f)
 
-bucket = config_data["input"]["bucket"]
-prefix = config_data["input"]["prefix"]
-kafka_bootstrap = config_data["kafka"]["bootstrap"]
-kafka_topic = config_data["kafka"]["topic"]
-fetch_interval = 5
-log_conf = config_data["logging"]["ingestion_path"]
+# Extract configuration parameters
+bucket = config_data["input"]["bucket"]                                                                                 # S3 bucket name
+prefix = config_data["input"]["prefix"]                                                                                 # prefix for location and time
+kafka_bootstrap = config_data["kafka"]["bootstrap"]                                                                     # kafka bootstrap server
+kafka_topic = config_data["kafka"]["topic"]                                                                             # kafka topic
+fetch_interval = 5                                                                                                      # Interval between processing files
+log_conf = config_data["logging"]["ingestion_path"]                                                                     # Logging configuration path
 
 # Initialize Logger
 log_path = init_log_file(log_conf)
 
-# kafka producer
+# Configure Kafka producer
 kafka_conf = {"bootstrap.servers": kafka_bootstrap}
 p = Producer(kafka_conf)
 
-# anonymous S3 client
+# Create an anonymous S3 client
 s3 = boto3.client('s3', config=boto3.session.Config(signature_version=botocore.UNSIGNED))
 
-# 1️List every file
+# List every file in the specified S3 bucket with the given prefix
 keys = []
 resp = s3.list_objects_v2(Bucket=bucket, Prefix=prefix)
 keys.extend([obj["Key"] for obj in resp.get("Contents", [])])
 log_event(log_path, "data_ingestion", "file_listed", len(keys), "success", "S3 file keys listed")
 print("Found", len(keys), "gzipped csv files")
 
-# --- Process each file separately ---
+# Process each file separately
 for key in tqdm(keys, desc="Sending one file at a time"):
     try:
+        # Get the object from S3
         obj = s3.get_object(Bucket=bucket, Key=key)
+
+        # Read the gzipped CSV file
         with gzip.GzipFile(fileobj=io.BytesIO(obj['Body'].read())) as f:
             reader = csv.DictReader(io.TextIOWrapper(f, encoding='utf-8'))
             count = 0
 
-            # Produce all rows in this file
+            # Produce all rows in this file to Kafka
             for row in reader:
                 json_row = json.dumps(row)
                 p.produce(kafka_topic, value=json_row)
@@ -52,9 +56,11 @@ for key in tqdm(keys, desc="Sending one file at a time"):
         log_event(log_path, "data_ingestion", "produce_kafka", count, "success", key)
         print(f"✅ Sent all messages from: {key}")
 
+        # Wait for the specified interval before processing the next file
         time.sleep(fetch_interval)
 
     except Exception as e:
+        # Log any errors that occur during processing
         log_event(log_path, "data_ingestion", "error", 0, "fail", str(e))
         print(f"❌ Error processing file {key}: {e}")
 
